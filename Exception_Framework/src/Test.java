21f4b3fcfd9c090e434cc5401a63b25a1ea4d982
Hari Krishna Prasad <harrycapper@gmail.com>
2013-05-02 22:19:02 +0530

    Adding Git Ignore file
    Test class and git ignore file added to repository

---
+++ Exception_Framework/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Am just testing my work for Git Source");
+	}
+
+}
